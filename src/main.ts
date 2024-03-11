a91d5cbd04e080f2f77737d24ada5ba364c0bf29
Samrach <kongsamrach.phan@cubetiqs.com>
2024-03-11 15:54:24 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
+injectSpeedInsights();
+
 bootstrap();
